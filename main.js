610ee4479a9e9285f9ca30b99aa51eed4de144a6
prem <sonanios2010@gmail.com>
2022-10-13 16:16:01 +0530

    committing changes for DOM commands

---
+++ main.js
@@ -0,0 +1,61 @@
+var form = document.getElementById('addform');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter-items');
+
+
+form.addEventListener('submit', addItem);
+
+itemList.addEventListener('click', removeItem);
+
+filter.addEventListener('keyup', filterItems);
+
+
+function addItem(e) {
+    e.preventDefault();
+
+
+    var newItem = document.getElementById('item').value;
+
+
+    var li = document.createElement('li');
+
+    li.className = 'list-group-item';
+
+    li.appendChild(document.createTextNode(newItem));
+
+
+    var deleteBtn = document.createElement('button');
+
+    deleteBtn.className = 'button delete';
+
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+    li.appendChild(deleteBtn);
+
+    itemList.appendChild(li);
+    console.log("yes")
+}
+
+function removeItem(e) {
+    if (e.target.classList.contains('delete')) {
+        if (confirm('Are You Sure?')) {
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
+
+function filterItems(e) {
+    var text = e.target.value.toLowerCase();
+
+    var items = itemList.getElementsByTagName('li');
+
+    Array.from(items).forEach((item) => {
+        var itemName = item.firstChild.textContent;
+        if (itemName.toLowerCase().indexOf(text) != -1) {
+            item.style.display = 'flex';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
